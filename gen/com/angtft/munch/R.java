647b9840e6ab383900fcb531fc446182cc19c50a
boggse <boggse@gmail.com>
2014-02-16 21:53:19 -0500

    choose role

--- gen/com/angtft/munch/R.java
+++ gen/com/angtft/munch/R.java
@@ -28,17 +28,19 @@ public final class R {
         public static final int shopper_icon=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000b;
+        public static final int action_settings=0x7f08000d;
         public static final int chooseRole=0x7f080008;
         public static final int email=0x7f080004;
         public static final int emailText=0x7f080003;
         public static final int forgot=0x7f080007;
-        public static final int hungryButton=0x7f080009;
+        public static final int hungryButton=0x7f08000a;
+        public static final int hungryPersonText=0x7f080009;
         public static final int loginText=0x7f080002;
         public static final int munchLogo=0x7f080000;
         public static final int password=0x7f080006;
         public static final int passwordText=0x7f080005;
-        public static final int shopperButton=0x7f08000a;
+        public static final int shopperButton=0x7f08000c;
+        public static final int shopperText=0x7f08000b;
         public static final int welcome=0x7f080001;
     }
     public static final class layout {
