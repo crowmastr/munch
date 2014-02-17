2e250c2e5c555ade412b62de3f580c41698fd5b9
boggse <boggse@gmail.com>
2014-02-16 21:14:17 -0500

    added new munch icon for app

--- gen/com/angtft/munch/R.java
+++ gen/com/angtft/munch/R.java
@@ -22,16 +22,19 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int icon=0x7f020001;
+        public static final int munch_logo=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
-        public static final int email=0x7f080003;
-        public static final int emailText=0x7f080002;
-        public static final int forgot=0x7f080006;
-        public static final int loginText=0x7f080001;
-        public static final int password=0x7f080005;
-        public static final int passwordText=0x7f080004;
-        public static final int welcome=0x7f080000;
+        public static final int action_settings=0x7f080008;
+        public static final int email=0x7f080004;
+        public static final int emailText=0x7f080003;
+        public static final int forgot=0x7f080007;
+        public static final int loginText=0x7f080002;
+        public static final int munchLogo=0x7f080000;
+        public static final int password=0x7f080006;
+        public static final int passwordText=0x7f080005;
+        public static final int welcome=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -46,6 +49,7 @@ public final class R {
         public static final int emailText=0x7f050004;
         public static final int forgotText=0x7f050007;
         public static final int loginText=0x7f050003;
+        public static final int munchLogo=0x7f050008;
         public static final int passwordText=0x7f050005;
         public static final int registerText=0x7f050006;
         public static final int welcome=0x7f050002;
