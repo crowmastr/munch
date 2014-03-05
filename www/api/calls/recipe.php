01a5ffaf2f49a259f6fa65a2c214a392c95a1617
Adam <amt12c@my.fsu.edu>
2014-03-05 03:40:56 -0500

    Add recipe list operation

--- www/api/calls/recipe.php
+++ www/api/calls/recipe.php
@@ -3,6 +3,22 @@ class recipe extends API
 {
 	public function call($data)
 	{
+		$this->validateExists($data, 'op');
+
+		$op = $data['op'];
+		if ($op == 'list')
+		{
+			$this->validateExists($data, 'ts');
+			$ts = $data['ts'];
+
+			$result = $this->db->execute("SELECT * FROM recipes WHERE `last_modified` > %d", $ts);
+			$resp = array();
+			while (($row = $result->fetch()))
+				$resp[] = $row;
+			return $resp;
+		}
+		else
+			throw new Exception("Unsupported op " . $op);
 	}
 }
 ?>
