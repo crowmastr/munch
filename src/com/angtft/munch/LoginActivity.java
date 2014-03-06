dabd810f5912803b7231a06a8a64effdc2eb736d
boggse <boggse@gmail.com>
2014-03-05 20:40:07 -0500

    -added methods to UserFunctions for: 	ilist.php 	ingredient.php 	recipe.php -cleaned up unused code in LoginActivity and RegisterActivity

--- src/com/angtft/munch/LoginActivity.java
+++ src/com/angtft/munch/LoginActivity.java
@@ -1,6 +1,5 @@
 package com.angtft.munch;
 
-import java.util.HashMap;
 import android.os.AsyncTask;
 
  
@@ -8,7 +7,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
  
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,7 +14,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
  
 import com.angtft.munch.library.DatabaseHandler;
 import com.angtft.munch.library.UserFunctions;
