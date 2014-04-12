ec0f261d334e58dc17d5b1906a5e95f6621fe81a
boggse <boggse@gmail.com>
2014-04-11 22:12:11 -0400

    Modifed ingredients query in Recipe.php to include ingredient name in query results.

--- www/api/calls/recipe.php
+++ www/api/calls/recipe.php
@@ -19,7 +19,7 @@ class recipe extends API
 			$this->validateExists($data, 'id');
 			$id = $data['id'];
 
-			$result = $this->db->execute("SELECT * FROM recipe_ingredients WHERE `recipe` = %d", $id);
+			$result = $this->db->execute("SELECT name, recipe_ingredients.amt, recipe_ingredients.info FROM ingredients RIGHT JOIN recipe_ingredients ON ingredients.id=recipe_ingredients.ingredient WHERE `recipe` = %d", $id);
 			if (!$result->ok() || $result->numRows() == 0)
 				throw new Exception("Invalid recipe id");
 
@@ -32,50 +32,50 @@ class recipe extends API
 		{
 			$this->validateExists($data, 'ingredients');
 			$ingredients = explode(",", $data['ingredients']);
-
+			
 			$possible_recipes = array();
 			$definite_recipes = array();
-
+			
 			foreach ($ingredients as $i)
 			{
 				$result = $this->db->execute("SELECT recipe FROM recipe_ingredients WHERE `ingredient` = %d", $i);
-
+				
 				$recipes = array();
 				while (($row = $result->fetch()))
 					$recipes[] = $row['recipe'];
-
+					
 				// recipes is all possible recipes we can create from this ingredient.
-
+				
 				if (empty($possible_recipes))
 					$possible_recipes = $recipes;
 				else
 					$possible_recipes = array_intersect($possible_recipes, $recipes);
 			}
-
+			
 			// possible_recipes contains recipes that we maybe can make, now lets see if we really have all of the stuff
 			foreach ($possible_recipes as $k => $r)
 			{
 				$result = $this->db->execute("SELECT ingredient FROM recipe_ingredients WHERE `recipe` = %d", $r);
-
+				
 				$ok = true;
 				while (($row = $result->fetch()))
 				{
 					$ing = $row['ingredient'];
-
+					
 					if (array_search($ing, $ingredients) === false)
 					{
 						$ok = false;
 						break;
 					}
 				}
-
+				
 				if ($ok)
 				{
 					unset($possible_recipes[$k]);
 					$definite_recipes[] = $r;
 				}
 			}
-
+			
 			$res = array();
 			$res["possible_recipes"] = $possible_recipes;
 			$res["definite_recipes"] = $definite_recipes;
